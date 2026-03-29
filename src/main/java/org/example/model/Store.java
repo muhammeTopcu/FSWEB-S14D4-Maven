27e989675bf384985cd67eaf3d38198c1106dee3
Muhammet Topcu <muhammetopcu06@gmail.com>
2026-03-29 15:04:19 +0300

    S14D4 done

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -2,11 +2,19 @@ package org.example.model;
 
 public class Store {
     public static void main(String[] args) {
+        ProductForSale[] products = new ProductForSale[3];
 
-        //listProducts();
+        products[0] = new Chocolate("Dark Chocolate", 50.0, "Bitter chocolate");
+        products[1] = new Coke("Coca Cola", 20.0, "Cold drink");
+        products[2] = new Bread("White Bread", 10.0, "Fresh bread");
+
+        listProducts(products);
+
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products) {
+            product.showDetails();
+        }
     }
 }
